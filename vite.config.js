eb62230a4a65ab47474f92862f02f821e1ec1695
thelocalgodd <splashvincentk@outlook.com>
2024-09-15 18:39:14 +0000

    tlbb

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/thelocalblogg",
   build: {
     outDir: "dist",
     assetsDir: "assets",
