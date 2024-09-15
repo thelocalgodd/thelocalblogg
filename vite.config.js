95cf055fe20ed4ad4d979bea8c63388feb20d5be
thelocalgodd <splashvincentk@outlook.com>
2024-09-15 18:43:32 +0000

    gg

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/thelocalblogg",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: true,
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-router-dom"],
-  },
 });
